af06a22b54128c81a3ea9fcef064015415ea7946
megu <170154@jc-21.jp>
2019-02-16 22:50:28 +0900

    bugari

--- test.java
+++ test.java
@@ -1,6 +1,12 @@
 public class test {
   public static void main(String[] args) {
     System.out.println("helloWork");
+    test teee = new test();
+    teee.insert();
+  }
+
+  public void insert() {
+    System.oyt.println("horayo;");
   }
 
 }
